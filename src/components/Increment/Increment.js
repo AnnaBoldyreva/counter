e3483d3a9888c21f3fcd035c92b59ee907fb8c1f
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-11-11 11:46:42 +0300

    refactoring css

--- src/components/Increment/Increment.js
+++ src/components/Increment/Increment.js
@@ -12,18 +12,11 @@ class Increment extends React.Component{
         if (this.props.state.currentValue !== this.props.state.maxValue){
             this.props.increment(this.props.state.currentValue)
         }
-        // else {
-        //     this.setState({
-        //         error: true
-        //     })
-        // }
     };
 
 
   render = () => {
       let isDisabled = this.props.state.currentValue === this.props.state.maxValue;
-
-      // let errorFilter = this.state.error ? 'error' : '';
     return (
         <div className={styles.inc}>
             <button onClick={this.incButtonClick} disabled={isDisabled} >inc</button>
